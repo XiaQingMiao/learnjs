a7b9ed35640f6c7b364471068cf4d6f5c87a688e
Lola Parik <lola@parik.ee>
2025-09-30 09:47:47 +0300

    math

--- script.js
+++ script.js
@@ -1,35 +1,29 @@
-    console.log('hello world!');
-var num = 1; // don't use var anymore
-let num2 = 2; 
-console.log(num2, num);
- let = 3.4; 
- console. log(typeof num); 
- num = 3;
- console.log(typeof num); 
- let text = 'Loola'; 
- console.log(typeof text); 
-let bool = true;
- console.log(typeof bool);
- bool = false;
-  console.log(typeof bool); 
- let nothing; 
-  console.log(typeof nothing);
-  nothing = undefined;
-  nothing = null; 
-   console.log(typeof nothing); 
-let array = [1, 2, 3, 4, 'sad', true, 3.4];
-  console.log(typeof array); 
-let obj = {
-    name: 'Loola',
-    age: 1001,
-    cast: ['Wow', 'Damn', 'Dayumn'],
-    data: {
-        phone: '123456789',
-        email: 'hfiu@gwhdf.lk'
-    }
-};
-console.log(typeof obj);
-function hello() {
-      console.log('hello'); 
-}
-  console.log(typeof hello); 
+let num = 1;
+console.log(num);
+num = num + 2;
+console.log(num);
+num = num - 1;
+console.log(num);
+num = num * 2;
+console.log(num);
+num = num / 3;
+console.log(num);
+num = 3;
+num = num ** 3;
+console.log(num);
+num = 9;
+num = num % 5; //modulo
+console.log(num);
+num += 2;
+num -= 2;
+num *= 2;
+num /= 2;
+num **= 2;
+num %= 2;
+console.log(num);
+num ++;
+console.log(num);
+num --;
+console.log(num);
+Math.sqrt(9);
+console.log(num);
