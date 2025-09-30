84d12fdbdb63525908315ff00feff2c49d5a1992
Lola Parik <lola@parik.ee>
2025-09-30 09:18:04 +0300

    add a lot of stuff

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
     console.log('hello world!');
+var num = 1; // don't use var anymore
+let num2 = 2; 
+console.log(num2, num);
+ let = 3.4; 
+ console. log(typeof num); 
+ num = 3;
+ console.log(typeof num); 
+ let text = 'Loola'; 
+ console.log(typeof text); 
+let bool = true;
+ console.log(typeof bool);
+ bool = false;
+  console.log(typeof bool); 
+ let nothing; 
+  console.log(typeof nothing);
+  nothing = undefined;
+  nothing = null; 
+   console.log(typeof nothing); 
+let array = [1, 2, 3, 4, 'sad', true, 3.4];
+  console.log(typeof array); 
+let obj = {
+    name: 'Loola',
+    age: 1001,
+    cast: ['Wow', 'Damn', 'Dayumn'],
+    data: {
+        phone: '123456789',
+        email: 'hfiu@gwhdf.lk'
+    }
+};
+console.log(typeof obj);
+function hello() {
+      console.log('hello'); 
+}
+  console.log(typeof hello); 
