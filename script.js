d3f6cdc813fc993c8b7f53e08ed130e8a8792374
Lola Parik <lola@parik.ee>
2025-10-28 09:01:20 +0200

    add timeout, interval, await, async examples

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-let nums = [6,3,5,7,9,4,2,8,1];
+setTimeout(() => {
+    console.log('later');
+},10_000); 
 
-nums.forEach(num => crossOriginIsolated.log(num));
-nums.forEach((num,i) => console.log(num,i));
+console.log('now');
 
-let even = nums.filtyer(num => num%2 ===0);
-console.log(even);
-let sum = nums.filtyer((sum, num) => sum+num);
-console.log(sum);
+setInterval(() => {
+    console.log('again');
+}, 10_000);
 
-let sorted = nums.sort((a,b) => {
-    if(a > b) {
-        return 1;
-
-    }
-    if(a < b) {
-        return -1;
-    }
-    return 0;
+let anwser = fetch('http://127.0.0.1:5500/').then(res => {
+    return res.text();
+}).then(text => {
+    console.log(text);
 });
-console.log(sorted);
-let sorted2 = nums.sort((a,b) => a-b);
-console.log(sorted2);
+console.log(anwser);
+let response = await fetch('http://127.0.0.1:5500/'); 
+console.log(response);
+let text = await response.text();
+console.log(text);
 
-let squares = nums.map(num => num*num);
-console.log(squares);
+async function getText(){
+   let response = await fetch('http://127.0.0.1:5500/'); 
+   console.log(response);
+   let text = await response.text();
+   console.log(text); 
+}
+
+await getText();
